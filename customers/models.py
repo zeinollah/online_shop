58e11cc1ef49cdfa1ba4543950e2c1ef6146acfc
Zein <zeinollahmozaffari@gmail.com>
2025-05-07 18:15:56 +0330

    make class for customer

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+
+class Customer(AbstractUser):
+    email = models.EmailField(_('email address'), unique=True, blank=False, null=False)
+    phone_number = models.CharField(_('phone number'), unique=True ,max_length=11, blank=False, null=False)
+    address = models.CharField(_('address'), max_length=100, blank=False, null=False)
+    post_code = models.IntegerField(_('post code'), max_length=10, blank=False, null=False)
+    birth_day = models.DateField(_('birth day'), blank=True, null=True)
+    created_at = models.DateTimeField(_('created at'), auto_now_add=True)
+    updated_at = models.DateTimeField(_('updated at'), auto_now=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+    class Meta:
+        ordering = ['-created_at']
--- online_shop/settings.py
+++ online_shop/settings.py
@@ -114,6 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'customers.Customer'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
