ff9b02b9cde51eff4c81eb9954d06ee63ecf12bc
Zein <zeinollahmozaffari@gmail.com>
2025-05-10 21:26:52 +0330

    use JWT in the project.

--- accounts/admin.py
+++ accounts/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 from .models import User
 
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['id', 'username', 'first_name', 'last_name', 'user_roll', 'email', 'phone_number', 'city', 'address']
+    list_display = ['id', 'username', 'first_name', 'last_name', 'birth_day' ,'user_roll',
+                    'email', 'phone_number', 'city', 'address','is_staff','is_active']
     list_filter = ['user_roll', 'city']
-    search_fields = ['email','last_name','phone_number','address']
+    search_fields = ['email', 'last_name', 'phone_number', 'address']
     ordering = ["-created_at"]
 admin.site.register(User, UserAdmin)
--- online_shop/settings.py
+++ online_shop/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'accounts',
     'customers',
     'orders',
@@ -138,3 +139,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+
+}
