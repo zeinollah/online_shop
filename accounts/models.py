99599d9e2e9bfbf739c7e58542abdcfad40418d3
Zein <zeinollahmozaffari@gmail.com>
2025-05-13 18:26:11 +0330

    correct typo!

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    USER_ROLL_CHOICES =(
+    USER_ROLE_CHOICES =(
         ('customer', 'customer'),
         ('seller', 'seller'),
     )
@@ -13,7 +13,7 @@ class User(AbstractUser):
     phone_number = models.CharField(_('Phone Number'), unique=True, max_length=11, blank=False, null=False)
     address = models.CharField(_('Address'), max_length=100, blank=False, null=False)
     city = models.CharField(_('City'), max_length=15, blank=False, null=False)
-    user_roll = models.CharField(_('Roll'),choices=USER_ROLL_CHOICES, max_length=10, blank=False, null=False)
+    user_roll = models.CharField(_('Role'),choices=USER_ROLE_CHOICES, max_length=10, blank=False, null=False)
     post_code = models.CharField(_('Post Code'), max_length=10, blank=False, null=False)
     birth_day = models.DateField(_('Birth Day'), blank=True, null=True)
     created_at = models.DateTimeField(_('created at'), auto_now_add=True)
