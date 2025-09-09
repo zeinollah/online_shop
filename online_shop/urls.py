265e1fb52c79dbbd3b82d81c399dc6f552c433c4
Zein <zeinollahmozaffari@gmail.com>
2025-09-09 20:12:09 +0330

    include customers urls

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/accounts/', include('accounts.urls')),
+    path('api/customers/', include('customers.urls')),
 ]
