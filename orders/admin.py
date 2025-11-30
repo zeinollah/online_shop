a9491a87d8fd7ffcf4d39ac1a6e6ceaffed49b7f
Zein <zeinollahmozaffari@gmail.com>
2025-11-30 18:36:13 +0330

    add admin class for order model

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Order
 
-# Register your models here.
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ["id", "customer", "order_number", "order_status",
+                    "shipping_phone", "shipping_city",
+                    "shipping_address", "shipping_post_code",
+                    "total_price", "payment_method", "is_paid","paid_at",
+                    "created_at", "updated_at"]
+
+    list_filter =["shipping_city", "order_status", "is_paid", "payment_method"]
+
+    search_fields = ["shipping_city", ""]
+
+    ordering =["-created_at"]
+
+admin.site.register(Order, OrderAdmin)
