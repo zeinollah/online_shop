04c615b2b632b8dabefc074b6f32021e5c6494ac
Zein <zeinollahmozaffari@gmail.com>
2025-12-02 11:27:48 +0330

    add IsCustomerProfileOwnerOrSuperuser

--- utils/permissions.py
+++ utils/permissions.py
@@ -1,11 +1,12 @@
 from rest_framework import permissions
 
+
 class IsProfileOwnerOrSuperuser(permissions.BasePermission):
     """
-    This class check the user before all actions to make sure user is admin or owner.
+    This class check the user before all actions to make sure user is admin or owner for seller.
     """
 
-    message = 'You DO NOT have permission to do this.'
+    message = 'You DO NOT have permission to do this action.'
 
     def has_object_permission(self, request, view, obj):
         user = request.user
@@ -13,6 +14,18 @@ class IsProfileOwnerOrSuperuser(permissions.BasePermission):
             return True
         return obj.seller.account == user
 
+
+class IsCustomerProfileOwnerOrSuperuser(permissions.BasePermission):
+    """
+    This class check the user before all actions to make sure user is admin or owner for customer.
+    """
+    message = 'You DO NOT have permission to do this action.'
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if user.is_superuser or user.is_staff:
+            return True
+        return obj.account == user
 
 
 class IsOrderOwnerOrSuperuser(permissions.BasePermission):
