6e06ff272562c63945178d6131bb7b9a05485dde
Zein <zeinollahmozaffari@gmail.com>
2025-12-31 22:33:18 +0330

    Write the seller discount create and info view class

--- discounts/views.py
+++ discounts/views.py
@@ -1,3 +1,77 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status, mixins
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+from utils.permissions import IsDiscountOwnerOrSuperuser
+from .models import SellerDiscount
+from .serializers import (
+    SellerDiscountCreateSerializer,
+    SellerDiscountUpdateSerializer,
+    SellerDiscountListSerializer,
+    SiteDiscountCreateSerializer,
+    SiteDiscountUpdateSerializer,
+    SiteDiscountListSerializer,
+    DiscountApplySerializer,
+    DiscountRemoveSerializer,
+    DiscountUsageListSerializer,
+    )
 
-# Create your views here.
+
+
+"""
+Seller Discount Views 
+"""
+class SellerDiscountCreateViewSet(viewsets.ModelViewSet):
+    queryset = SellerDiscount.objects.all()
+    serializer_class = SellerDiscountCreateSerializer
+    http_method_names = ['post']
+    permission_classes = [IsAuthenticated]
+
+    def create(self, request, *args, **kwargs):
+        if not hasattr(request.user, 'seller_profile'):
+            return Response(
+                {"message": "Complete the profile is required to create Discount code."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save(seller=request.user.seller_profile)
+
+        return Response({
+            "message": "Your Discount has been created successfully",
+            "data": serializer.data
+        }, status=status.HTTP_201_CREATED)
+
+
+
+class SellerDiscountInfoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = SellerDiscount.objects.all()
+    serializer_class = SellerDiscountListSerializer
+    permission_classes = [IsAuthenticated, IsDiscountOwnerOrSuperuser]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser or user.is_staff:
+            return SellerDiscount.objects.all()
+
+        if hasattr(user, 'seller_profile'):
+            return SellerDiscount.objects.filter(seller=user.seller_profile)
+
+        else:
+            return False
+
+
+
+class SellerDiscountUpdateViewSet(mixins.UpdateModelMixin, viewsets.GenericViewSet):
+    pass
+
+
+
+class SellerDiscountRemoveViewSet(mixins.DestroyModelMixin, viewsets.GenericViewSet):
+    pass
+
+
+
+
+"""
+Site Discount Views 
+"""
