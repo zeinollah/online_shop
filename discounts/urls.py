51583cfb37edd6466f4fa511dac7553251b8141b
Zein <zeinollahmozaffari@gmail.com>
2026-01-05 16:27:05 +0330

    complete Seller Discount urls

--- discounts/urls.py
+++ discounts/urls.py
@@ -1,9 +1,16 @@
 from rest_framework.routers import DefaultRouter
-from .views import (SellerDiscountCreateViewSet,
-                    SellerDiscountInfoViewSet,
-                    )
+from .views import (
+    SellerDiscountCreateViewSet,
+    SellerDiscountUpdateViewSet,
+    SellerDiscountInfoViewSet,
+    SellerDiscountDeleteViewSet,
+    )
 
 router = DefaultRouter()
-router.register('create-seller-discount', SellerDiscountCreateViewSet, basename='create_discount')
-router.register('seller-discount-info', SellerDiscountInfoViewSet, basename='info_discount')
+
+router.register('seller-discount-info', SellerDiscountInfoViewSet, basename='seller_discount_info')
+router.register('create-seller-discount', SellerDiscountCreateViewSet, basename='create_seller_discount')
+router.register('update-seller-discount', SellerDiscountUpdateViewSet, basename='update_seller_discount')
+router.register('delete-seller-discount', SellerDiscountDeleteViewSet, basename='delete_seller_discount')
+
 urlpatterns = router.urls
