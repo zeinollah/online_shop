dc0dbd9609e2d6cf46724034054d2bf702825f2d
Zein <zeinollahmozaffari@gmail.com>
2026-02-11 16:15:04 +0330

    created DiscountApplyViewSet

--- discounts/views.py
+++ discounts/views.py
@@ -2,13 +2,16 @@ from rest_framework import viewsets, status, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from utils.permissions import IsDiscountOwnerOrSuperuser
-from .models import SellerDiscount, SiteDiscount
+from .models import SellerDiscount, SiteDiscount, DiscountUsage
 from rest_framework.exceptions import PermissionDenied
 from .serializers import (
     SellerDiscountListSerializer,
     SellerDiscountCreateSerializer,
-    SellerDiscountUpdateSerializer, SiteDiscountCreateSerializer, SiteDiscountListSerializer,
+    SellerDiscountUpdateSerializer,
+    SiteDiscountCreateSerializer,
+    SiteDiscountListSerializer,
     SiteDiscountUpdateSerializer,
+    DiscountApplySerializer,
 )
 
 
@@ -169,3 +172,63 @@ class SiteDiscountDeleteViewSet(mixins.DestroyModelMixin, viewsets.GenericViewSe
             status=status.HTTP_200_OK
         )
 
+
+"""
+Usage Discount views
+"""
+class DiscountApplyViewSet(viewsets.GenericViewSet):
+    serializer_class = DiscountApplySerializer
+    permission_classes = [IsAuthenticated]
+
+
+    def create(self, request, *args, **kwargs):
+        if not hasattr(request.user, 'customer_profile'):
+            return Response(
+                {"message" : "Customer profile required"},
+                        status=status.HTTP_400_BAD_REQUEST
+            )
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        discount = serializer.validated_data['discount']
+        order_item = serializer.validated_data['order_item']
+        customer = serializer.validated_data['customer']
+
+        item_subtotal = order_item.price * order_item.quantity
+
+        if discount.discount_type == 'percentage':
+            discount_amount = (item_subtotal * int(discount.value)) / 100
+        else:
+            discount_amount = min(discount.value, item_subtotal)
+
+        discount_usage = DiscountUsage.objects.create(
+            seller_discount = discount if isinstance(discount, SellerDiscount) else None,
+            site_discount = discount if isinstance(discount, SiteDiscount) else None,
+            discount_code = discount.code,
+            discount_type = discount.discount_type,
+            discount_value = discount.value,
+            customer = customer,
+            order = order_item.order,
+            order_item = order_item,
+            discount_amount = discount_amount,
+        )
+
+        order_item.discount = discount_amount
+        order_item.save()
+
+        discount.is_used = True
+        discount.used_by = customer
+        discount.save()
+
+        order_item.order.calculate_total()
+
+        return Response({
+            "message": "Discount applied successfully.",
+            "discount_code": discount_usage.discount_code,
+            "discount_amount": str(discount_usage.discount_amount),
+            "order_item_id": discount_usage.order_item.id,
+            "new_subtotal": str(order_item.subtotal)
+            },
+            status=status.HTTP_200_OK
+        )
