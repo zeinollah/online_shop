2927e5cad365bce3de2058dff95084956ec800a9
Zein <zeinollahmozaffari@gmail.com>
2026-02-11 16:19:58 +0330

    add path for apply discount on order item

--- discounts/urls.py
+++ discounts/urls.py
@@ -8,18 +8,24 @@ from .views import (
     SiteDiscountInfoViewSet,
     SiteDiscountUpdateViewSet,
     SiteDiscountDeleteViewSet,
+    DiscountApplyViewSet,
     )
 
 router = DefaultRouter()
-
-router.register('seller-discount-info', SellerDiscountInfoViewSet, basename='seller_discount_info') # Seller Urls
+"""Seller Discount Urls"""
+router.register('seller-discount-info', SellerDiscountInfoViewSet, basename='seller_discount_info')
 router.register('create-seller-discount', SellerDiscountCreateViewSet, basename='create_seller_discount')
 router.register('update-seller-discount', SellerDiscountUpdateViewSet, basename='update_seller_discount')
 router.register('delete-seller-discount', SellerDiscountDeleteViewSet, basename='delete_seller_discount')
 
-router.register('site-discount-info', SiteDiscountInfoViewSet, basename='site_discount_info') # Site Urls
+"""Site Discount Urls"""
+router.register('site-discount-info', SiteDiscountInfoViewSet, basename='site_discount_info')
 router.register('create-site-discount', SiteDiscountCreateViewSet, basename='create_site_discount')
 router.register('update-site-discount', SiteDiscountUpdateViewSet, basename='update_site_discount')
 router.register('delete-site-discount', SiteDiscountDeleteViewSet, basename='delete_site_discount')
+
+"""Discount Usage Urls"""
+router.register('apply-discount', DiscountApplyViewSet, basename='apply_discount')
+
 
 urlpatterns = router.urls
