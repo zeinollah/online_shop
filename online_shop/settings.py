01abda42b03c8520a95bd73f42cf6015c007fcea
Zein <zeinollahmozaffari@gmail.com>
2026-02-15 11:43:58 +0330

    Added token_blacklist to INSTALLED_APPS

--- online_shop/settings.py
+++ online_shop/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
     'utils',
     'accounts',
     'customers',
