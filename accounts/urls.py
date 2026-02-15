2d02a3482998e70a8a3ccb506c7834b527ff2af4
Zein <zeinollahmozaffari@gmail.com>
2026-02-15 12:05:49 +0330

    added path to login and logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,13 +1,23 @@
 from rest_framework.routers import DefaultRouter
-from .views import (RegistrationViewSet,
-                    UserInfoViewSet,
-                    UpdateUserViewSet,
-                    DeleteUserViewSet)
+from .views import (
+    RegistrationViewSet,
+    UserInfoViewSet,
+    UpdateUserViewSet,
+    DeleteUserViewSet,
+    LoginViewSet,
+    LogoutViewSet,
+)
 
 router = DefaultRouter()
+"""CRUD Patch"""
 router.register(r'register', RegistrationViewSet, basename='register')
 router.register(r'userinfo', UserInfoViewSet, basename='userinfo')
 router.register(r'user-update', UpdateUserViewSet, basename='user-update')
 router.register(r'user-delete', DeleteUserViewSet, basename='user-delete')
+
+
+"""Login / Logout Patch"""
+router.register(r'login', LoginViewSet, basename='login')
+router.register(r'logout', LogoutViewSet, basename='logout')
 
 urlpatterns = router.urls
