44fb02419409496d9ebbd847d6965d87e0a686a3
Zein <zeinollahmozaffari@gmail.com>
2026-02-16 12:08:24 +0330

    i have no idea what change the git just track change file in urls but there is nothing

--- online_shop/urls.py
+++ online_shop/urls.py
@@ -38,7 +38,6 @@ schema_view = get_schema_view(
     ),
     public=True,
     permission_classes=[AllowAny],
-    authentication_classes=[],
 )
 urlpatterns = [
     path('admin/', admin.site.urls),
