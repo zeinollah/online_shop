8785e0af56d0e4b0bac3112d5c0e76d67a96e72d
Zein <zeinollahmozaffari@gmail.com>
2026-02-24 15:47:22 +0330

    Added for wallet-info, transactions, transactions-history path

--- customers/urls.py
+++ customers/urls.py
@@ -1,16 +1,26 @@
 from rest_framework.routers import DefaultRouter
-from .views import (CustomerProfileCreateViewSet,
-                    CustomerProfileUpdateViewSet,
-                    CustomerProfileInfoViewSet,
-                    CustomerProfileDetailViewSet
-                    )
+from .views import (
+    CustomerProfileCreateViewSet,
+    CustomerProfileUpdateViewSet,
+    CustomerProfileInfoViewSet,
+    CustomerProfileDetailViewSet,
+    WalletInfoViewSet,
+    TransactionCreateViewSet,
+    TransactionHistoryViewSet,
+    )
 
 router = DefaultRouter()
+
+"""Customer Path"""
 router.register(r'create-profiles', CustomerProfileCreateViewSet, basename='create-profiles')
 router.register(r'update-profiles', CustomerProfileUpdateViewSet, basename='update-profiles')
 router.register(r'info-profiles', CustomerProfileInfoViewSet, basename='info-profiles')
 router.register(r'delete-profiles', CustomerProfileDetailViewSet, basename='delete-profiles')
 
+"""Wallet Path"""
+router.register(r'wallet-info', WalletInfoViewSet, basename='wallet-info')
+router.register(r'transactions', TransactionCreateViewSet, basename='transactions')
+router.register(r'transactions-history', TransactionHistoryViewSet, basename='transactions-history')
 urlpatterns = router.urls
 
 # TODO = Add -customers- into urls and remove s from profiles
