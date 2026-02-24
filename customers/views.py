1df11510d0c6520b786eee4af0659310ae04a89f
Zein <zeinollahmozaffari@gmail.com>
2026-02-24 15:42:17 +0330

    Created WalletInfoViewSet, TransactionCreateViewSet, TransactionHistoryViewSet

--- customers/views.py
+++ customers/views.py
@@ -1,17 +1,21 @@
-from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, status, mixins
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from utils.permissions import IsCustomerProfileOwnerOrSuperuser
-from .models import CustomerProfile
-from .serializers import (CustomerProfileSerializer,
-                          CustomerProfileUpdateSerializer,
-                          )
+from .models import CustomerProfile, Wallet, Transaction
+from .serializers import (
+    CustomerProfileSerializer,
+    CustomerProfileUpdateSerializer,
+    WalletInfoSerializer,
+    TransactionCreateSerializer,
+    )
 
 
 
 
-
+"""
+Customer Profile Views
+"""
 class CustomerProfileCreateViewSet(viewsets.ModelViewSet):
     queryset = CustomerProfile.objects.all()
     serializer_class = CustomerProfileSerializer
@@ -80,3 +84,60 @@ class CustomerProfileDetailViewSet(mixins.DestroyModelMixin, viewsets.GenericVie
             "message: Profile deleted successfully",
             status=status.HTTP_200_OK
         )
+
+
+
+"""
+Wallet Views
+"""
+
+class WalletInfoViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Wallet.objects.all()
+    serializer_class = WalletInfoSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser or user.is_staff:
+            return Wallet.objects.all()
+        return Wallet.objects.filter(customer=user.customer_profile)
+
+
+
+class TransactionCreateViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionCreateSerializer
+    permission_classes = [IsAuthenticated]
+    http_method_names = ['post']
+
+    def create(self, request, *args, **kwargs):
+        wallet = request.user.customer_profile.wallets
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        transaction = serializer.save(wallet=wallet)
+
+        if transaction.transaction_type == "top_up":
+            wallet.balance += transaction.amount
+
+        elif transaction.transaction_type == "payment":
+            wallet.balance -= transaction.amount
+
+        wallet.save()
+
+        return Response(
+            {"message": "Transaction completed successfully"},
+            status=status.HTTP_201_CREATED
+        )
+
+
+
+class TransactionHistoryViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Transaction.objects.all()
+    serializer_class = TransactionCreateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_superuser or user.is_staff:
+            return Transaction.objects.all()
+        return Transaction.objects.filter(wallet=user.customer_profile.wallets).order_by("-created_at")
